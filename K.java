e4c42ede4424725592195aec0490a36e48a8bed3
nicholus77 <deepakfire15@gmail.com>
2026-02-13 09:58:12 +0530

    Add files via upload

---
+++ K.java
@@ -0,0 +1,26 @@
+
+class O{
+    void add(int a,int b, int c)
+    {
+        System.out.println("SUM : "+(a+b+c));
+    }
+
+      void add(int a,int b)
+    {
+        System.out.println("SUM : "+(a+b));
+    }
+
+    class K{
+
+        
+public static void main(String[] args) 
+{
+
+ O meerukam = new O();
+          meerukam.add(16,90,90);
+          meerukam.add(90,90);
+          
+             
+    }
+}
+
